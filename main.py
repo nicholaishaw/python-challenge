4b75e83c39873ea882d6115223273531993596da
nicholaishaw <135463220+nicholaishaw@users.noreply.github.com>
2023-07-05 22:46:49 -0400

    Update main.py
    Added Comments

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 import csv
 import pandas as pd
 
+#Creates the path for both of the CSV files
 pybank_csv = os.path.join("starter_code", "pybank", "Resources", "budget_data.csv")
 pypoll_csv = os.path.join("starter_code", "pypoll", "Resources", "election_data.csv")
 
+#Sets the net_total variable to 0
 net_total = 0
 
+#Opens the csv files, creates a header, and the next function allows the code to be read after the header
 with open(pybank_csv) as csvfile:
     pybank = csv.reader(csvfile)
     header = next(pybank)
@@ -23,6 +26,7 @@ with open(pybank_csv) as csvfile:
     print(f"Total Months: {Month_Count}")
     print(f"Net Total: {net_total}")
 
+#Opens the pypoll csv file and iterates through each row
 with open(pypoll_csv) as csvfile:
     pypoll = csv.reader(csvfile)
     header = next(pypoll)
