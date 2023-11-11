ce9caebd7cc6b4bb8c9c219cc485362f9b131bfa
nicholaishaw@gmail.com <nicholaishaw@gmail.com>
2023-11-11 17:07:38 -0500

    added results file, updated references, and redirected the output to print in the results file.

--- Solution.py
+++ Solution.py
@@ -1,11 +1,16 @@
-#Tutor Mark Fullton assisted with the development of the logic an syntax of the code in this document
+#Tutors Marc Calache and Mark Fullton assisted with the development of the logic an syntax of the code in this document
+#The idea to sum a row on lines #19-21 originated from a Stack Overflow thread (Link: https://stackoverflow.com/questions/13517080/sum-a-csv-column-in-python, the author is named Martijn Pieters, and the date of the publication is November 22, 2012).
+#The syntax to export python results to a text file was provided from: https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
 
 import os
 import csv
 
+results_path = 'results.txt'
+results = open(results_path, 'w')
 pybank_csv = os.path.join('PyBank', 'Resources', 'budget_data.csv')
 
 net_total = 0
+
 
 with open(pybank_csv) as csvfile:
     pybank = csv.reader(csvfile)
@@ -37,15 +42,17 @@ with open(pybank_csv) as csvfile:
     greatest_increase = month_variable[max_index]
     greatest_decrease = month_variable[min_index]
 
-    print('Financial Analysis')
-    print('------------')
-    print(f'Total Months: {Month_Count}')
-    print(f'Net Total: {profits_sum}')
-    print(f'Average Change: {mean}')
-    print(f'Greatest Increase: {greatest_increase} (${max_value})')
-    print(f'Greatest Decrease: {greatest_decrease} (${min_value})')
-    print('------------')
-    print('')
+    print('Financial Analysis', file=results)
+    print('------------', file=results)
+    print(f'Total Months: {Month_Count}', file=results)
+    print(f'Net Total: {profits_sum}', file=results)
+    print(f'Average Change: {mean}', file=results)
+    print(f'Greatest Increase: {greatest_increase} (${max_value})', file=results)
+    print(f'Greatest Decrease: {greatest_decrease} (${min_value})', file=results)
+    print('------------', file=results)
+    print('', file=results)
+    print('', file=results)
+    print('', file=results)
 
 #--------------------
 
@@ -64,18 +71,20 @@ with open(pypoll_csv) as csvfile:
             candidate_dic[candidate] = 1
         total_votes = total_votes + 1
 
-print('Election Results')
-print('------------')
-print(f'Total Votes: {total_votes}')
-print('------------')
+print('Election Results', file=results)
+print('------------', file=results)
+print(f'Total Votes: {total_votes}', file=results)
+print('------------', file=results)
 for candidate in candidate_dic:
     candidate_votes = candidate_dic[candidate]
     candidate_percentage = (candidate_votes / total_votes) * 100
-    print(f'{candidate}: {candidate_percentage}% ({candidate_votes})')
+    print(f'{candidate}: {candidate_percentage}% ({candidate_votes})', file=results)
 
     if candidate_votes > comparison_votes:
         comparison_votes = candidate_votes
         winner = candidate
 
-print('------------')
-print(f'Winner: {winner}')
+print('------------', file=results)
+print(f'Winner: {winner}', file=results)
+print('------------', file=results)
+results.close()
